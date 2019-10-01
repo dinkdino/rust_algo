fc8630a2f999803aafc1e190f0b992eb616d28dd
Hrishikesh Sawant <hrishikesh.sawant1@tcs.com>
2019-10-01 08:40:11 +0800

    Implement sink on heap

--- src/heap.rs
+++ src/heap.rs
@@ -6,19 +6,34 @@ pub struct Heap<T: PartialOrd, F> {
 }
 
 impl <T: PartialOrd, F> Heap<T, F>
-    where F: FnMut(&T, &T) -> cmp::Ordering {
+    where F: Fn(&T, &T) -> cmp::Ordering {
 
     pub fn new(compare: F) -> Heap<T, F> {
         Heap { nodes: Vec::new(), compare }
     }
 
     fn sink(&mut self, index: usize) {
-        if index == self.nodes.len() { return }
+        let len = self.nodes.len();
+        if index == len { return }
 
-        let leftChildIndex = (index * 2) + 1;
-        let rightChildIndex = (index * 2) + 2;
+        let left_child_index = (index * 2) + 1;
+        let right_child_index = (index * 2) + 2;
             
         let mut c_index = index;
+
+        if c_index < len && (self.compare)(&self.nodes[c_index], &self.nodes[left_child_index]) == cmp::Ordering::Greater {
+            c_index = left_child_index;
+        }
+
+        if c_index < len && (self.compare)(&self.nodes[c_index], &self.nodes[right_child_index]) == cmp::Ordering::Greater {
+            c_index = right_child_index;
+        }
+
+        if c_index == index { return }
+
+        self.nodes.swap(c_index, index);
+
+        self.sink(c_index);
     }
 
     pub fn push(&mut self, value: T) {
